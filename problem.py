498e2c15f900008fb75532c92c511abdcac04616
Your Name <you@example.com>
2026-01-12 11:46:20 +0600

    solve some chili problem

---
+++ problem.py
@@ -0,0 +1,33 @@
+
+# !! Write a program to input 2 number & print their sum.
+
+# number1 = int(input("Enter your first number : "));
+# number2 = int(input("Type second number : "));
+
+# print("sum of the number :--> ", number1+number2);
+
+# !!2 Wap to input side of a square & print its area
+
+# side = float(input("Enter square side : "))
+
+# print("area=" , side**2);
+
+# !! Wap to input 2 floating numbers & print their avarage
+
+# float_number1 = float(input("First number :"))
+# float_number2 = float(input("second number :"))
+
+# print("Avarage number-->" , (float_number1+float_number2)/2);
+
+# !!WAP to input 2 int number , a and b
+# !!Print true if a is >= b, if not prit false
+
+a = int(input("Enter a ="))
+b = int(input("Enter b ="))
+
+if(a>=b):{
+ print(True)
+}
+else: {
+    print(False)
+}
