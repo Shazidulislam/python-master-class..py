1d9c560381e6a2069b5ddfc9d203d58a2fbdf0b4
Your Name <you@example.com>
2026-01-12 11:14:45 +0600

    first commit

---
+++ lecture1.py
@@ -0,0 +1,64 @@
+
+# ! print output
+# print("Hello World");
+
+# print("My name is Shazid" , "My age 20");
+
+# !variable
+name = "Shazid";
+age = 20;
+price = 100.55;
+
+# print("My name is : ", name);
+
+# print("My age is : " , age);
+
+# print("Product price : " , price);
+
+# !! redelar vaiable
+
+age2 = age;
+
+# *Rules for Identifiers
+
+""""
+# !!Data Types
+
+Integers = 22 , -20 , 0
+
+String = "string"
+
+Float = 20.39 , -44.567
+
+Boolean = True , False
+
+Non  a = None
+
+"""
+
+# print(type(name))
+# print(type(age2))
+# print(type(price))
+
+old = False;
+a =None;
+
+# print(type(old));
+
+# print(type(a));
+
+"""
+#!! Keywords
+
+# !!Keywords are reserved words in python.
+
+# *False should be uppercase
+
+"""
+
+# Print Sum
+
+a =400;
+b=300;
+
+# print("sum of a and b : " , a+b);
---
+++ lecture2.py
@@ -0,0 +1,73 @@
+
+
+"""
+# !!Types of Operators
+# ? An operator is a symbol that performs a certain operation between operands.
+
+# !! Arithmetic Operators ( + , - , * , / , % , ** )
+
+# **Relational / Comparison Operators ( == , != , > , < , >= , <= )
+
+# !!Assignment Operators ( = , +=, -= ,*= , /= , %= ,**= )
+
+# ?Logical Operators ( not , and , or )
+
+
+"""
+
+# !! Arithmetic Operators ( + , - , * , / , % ,** )
+
+# a = 5
+# b = 3
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b) #reminder
+# print(a**b)  #power a^b
+
+# **Relational / Comparison Operators ( == , != , > , < , >= , <= )
+
+a = 20;
+b = 30;
+
+# print(a==b) #False
+# print(a!=b) #True
+# print(a>b) #False
+# print(a<b) #True
+# print(a>=b) #False
+# print(a<=b) #True
+
+
+# !!Assignment Operators ( = , +=, -= ,*= , /= , %= ,**= )
+
+num = 10;
+num +=10;
+
+# print("Num : ",num);
+num -= 5;
+
+# print("Num Sub : ",num);
+
+num *= 5;
+# print("Num mul : ", num)
+
+num /= 5;
+# print("Num Div : ", num)
+
+
+num %= 2;
+# print("Num % : ", num)
+
+nums =5;
+nums **= 2;
+# print("Nums ** : ", nums)
+
+
+
+# !Logical Operators ( not , and , or )
+
+print(not False);
+print(not True);
+print(not (4>5));
