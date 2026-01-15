190c9ba70b2a51a85386c29f583f5b867f0ec81a
Your Name <you@example.com>
2026-01-15 19:39:27 +0600

    learn if and else & string function

---
+++ chapter2/ifandelse.py
@@ -0,0 +1,34 @@
+
+
+"""
+
+#!Conditional Statements
+
+#**if-elif-else (SYNTAX)
+
+if(condition) :
+
+Statement1
+
+elif(condition):
+
+Statement2
+
+else:
+
+StatementN
+
+"""
+
+
+age = 19 ;
+
+if(age>18):{
+    print("You can vote & apply for license")
+}
+elif(age==18):{
+    print("You are beginer now")
+} 
+
+else:
+    print("Not now")    
---
+++ chapter2/problemSolving.py
@@ -0,0 +1,14 @@
+
+#! WAp to input uuser's first name & print its length.
+
+name = input("Type your first name: ")
+
+# print(name , len(name))
+
+
+
+#*WAP to find the occurence of "$" in a String.
+
+str = "$ This is a symbole $ , its called $Dolor"
+
+print(str.count("$"))
