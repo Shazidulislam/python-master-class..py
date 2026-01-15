3f747a16806a5bb33f289e8e75bcb587f2ebf365
Your Name <you@example.com>
2026-01-15 19:29:20 +0600

    learn if and elsen

--- chapter2/ifandelse.py
+++ chapter2/ifandelse.py
@@ -23,12 +23,22 @@ StatementN
 
 age = 19 ;
 
-if(age>18):{
+if(age>18):
     print("You can vote & apply for license")
-}
-elif(age==18):{
+    print("You can drive")
+elif(age==18):
     print("You are beginer now")
-} 
-
 else:
     print("Not now")    
+
+
+marks = int(input("Studen marks :  "))
+
+if(marks >= 90):
+    print("Grad is A++")
+elif(90 > marks & marks >= 80):
+    print("Grad is B")
+elif(80 >  marks & marks >= 70):
+    print("Grad is C")    
+else:
+    print("Grad is D")
--- chapter2/problemSolving.py
+++ chapter2/problemSolving.py
@@ -1,7 +1,7 @@
 
 #! WAp to input uuser's first name & print its length.
 
-name = input("Type your first name: ")
+# name = input("Type your first name: ")
 
 # print(name , len(name))
 
@@ -12,3 +12,32 @@ name = input("Type your first name: ")
 str = "$ This is a symbole $ , its called $Dolor"
 
 print(str.count("$"))
+
+
+#! WAp to check if a number entered by th user is odd or even
+
+number = int(input("Enter a number : "));
+
+if(number%2==0):
+    print("Even Number")
+else:
+    print("Odd Number")
+
+
+#* WAP to find the greatest of 3 number enered by the user
+
+a = int(input("enter a number: "))
+b = int(input("enter b number: "))
+c = int(input("enter c number: "))
+
+if(a>b & a>c):
+    print(a , "Is greatest number")
+elif(b>c):
+    print(b, "Is greatest number")
+else:
+        print(c , "Is greatest number")
+
+
+
+#? WAp to check if a number is a multipule of 7 or not.
+
